56d868b3c53e114cba336bb368a973b96004d1cd
ksenia <ksusamaleeva05@gmail.com>
2024-09-26 13:04:05 +0300

    внедрение реакт компонентов в виде карточек на страницу

--- src/App.js
+++ src/App.js
@@ -2,6 +2,27 @@ import Card from './components/Card'
 import Header from './components/Header'
 import RightBasket from './components/RighrBasket';
 
+
+const arr = [
+  { title: "Bearbrick & Sesame Street 'Cookie Monster Costume'", price: 317, imageUrl: "/img/toys/bearbrick1.png"},
+  { title: "Bearbrick X De scipable Me 'Kevin'", price: 616, imageUrl: "/img/toys/bearbrick2.png"},
+  { title: "Bearbrick FCReal Bristol X COCA-COLA", price: 270, imageUrl: "/img/toys/bearbrick3.png"},
+  { title: "Bearbrick x Dark Knight 'The Joker'", price: 302, imageUrl: "/img/toys/bearbrick4.png"},
+  { title: "Bearbrick Squid Game 'Two-Toned Collectible Statue'", price: 156, imageUrl: "/img/toys/bearbrick5.png"},
+  { title: "Bearbrick X Care Bears Cheer 'Bear Costume'", price: 313, imageUrl: "/img/toys/bearbrick6.png"},
+  { title: "Bearbrick 'XLARGE'", price: 154, imageUrl: "/img/toys/bearbrick7.png"},
+  { title: "Bearbrick Bape x COMME des GARCONS 'Camo Shark' ", price: 190, imageUrl: "/img/toys/bearbrick8.png"},
+  { title: "Bearbrick Bape R X Alpha 'Camo Shark'", price: 419, imageUrl: "/img/toys/bearbrick9.png"},
+  { title: "Bearbrick X Tom And Jerry 'Tom'", price: 333, imageUrl: "/img/toys/bearbrick10.png"},
+  { title: "Bearbrick X Tom And Jerry 'Jerry'", price: 333, imageUrl: "/img/toys/bearbrick11.png"},
+  { title: "Bearbrick x Pushead 'Jell-C Clear'", price: 451, imageUrl: "/img/toys/bearbrick12.png"},
+  { title: "Bearbrick 'Elmo Costume'", price: 192, imageUrl: "/img/toys/bearbrick13.png"},
+  { title: "Bearbrick x Marvel 'Iron Man'", price: 240, imageUrl: "/img/toys/bearbrick14.png"},
+  { title: "Bearbrick 'So... Hi!'", price: 190, imageUrl: "/img/toys/bearbrick15.png"},
+  { title: "Bearbrick Gremlins 'Gizmo Costume'", price: 343, imageUrl: "/img/toys/bearbrick16.png"}
+]
+
+
 function App() {
   return (
     <div className="wrapper">
@@ -20,22 +41,20 @@ function App() {
         </div>
 
         <div className="toys">
-          <Card />
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />  
-          <Card />  
+
+         {/*передаем элементы массива в аргументы функции Card
+         т.е. берем объекты массива и превращаем в реакт компоненты*/}
+         
+         {arr.map((obj) => (
+          <Card 
+            title={obj.title}
+            price={obj.price}
+            imageUrl={obj.imageUrl}
+            onClickPlus={ () => console.log('plus')}
+            onClickFavourite={ () => console.log('like') }
+          />
+         ))}
+           
         </div>
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,17 @@
-function Card() {
+function Card(props) {
     return(
         <div className="card">
-          <div className="favourite">
+          <div className="favourite" onClick={props.onClickFavourite}>
             <img src='img/heart_unliked.svg' alt="heart_icon"/>
           </div>
-          <img src="/img/toys/bearbrick1.png" alt="bearbrick_1" width={200} height={200}/>
-          <p>Bearbrick & Sesame Street 'Cookie Monster Costume'</p>
+          <img src={props.imageUrl} alt="bearbrick_1" width={200} height={200}/>
+          <p>{props.title}</p>
           <div className="cardBottom">
             <div className="cardBottom2">
               <span>Price:</span>
-              <b> 317$</b>
+              <b>{props.price}$</b>
             </div>
-            <button className="button">
+            <button className="button" onClick={props.onClickPlus}>
               <svg width="11" height="11" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M10.6653 5.13128H7.20219V1.66827C7.20219 0.332907 5.13118 0.332907 5.13118 1.66827V5.13128H1.66805C0.332981 5.13128 0.332981 7.20221 1.66805 7.20221H5.13118V10.6652C5.13118 12.0006 7.20219 12.0006 7.20219 10.6652V7.20221H10.6653C12.0006 7.20221 12.0006 5.13128 10.6653 5.13128Z" fill="#D3D3D3"/>
               </svg>
